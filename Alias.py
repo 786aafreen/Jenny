0f0b5237300517eddf3d4ed33cc3b1b02df68717
786@@freen <legend.rafi095@gmail.com>
2023-02-01 09:55:13 +0530

    Done

---
+++ Alias.py
@@ -0,0 +1,5 @@
+a = '''And let you set the Pace,
+          I'/m not thinking straight'''
+
+
+print(a)
--- Poo.py
+++ Poo.py
@@ -1,7 +1,7 @@
 import pyttsx3
 print("Hello Universe Let's habve a ride Wooooooooooo")
-a=Aafreen
-b=Khan
+a='Aafreen'
+b='Khan'
 c=a+b
 print(c)
 print('''Wowwwwww
